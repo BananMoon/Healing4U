ac3842b958b1cf78a90bf722bc1ce93e5083f603
moonz <banan99@naver.com>
2021-08-23 01:45:42 +0900

    modify: weatherAPI & aws Server

--- routes/index.js
+++ routes/index.js
@@ -14,16 +14,26 @@ console.log('점수: ', rating);
 router.get('/', function(req, res) {
   let dataList = [];
   let today = new Date();
-  let month_params = today.getMonth()+1;
+  let month = today.getMonth()+1;
+  if (3<=month<=5) {
+    month_param = 0;    //봄
+  } else if (6<=month<=8) {
+    month_param = 1;    //여름
+  } else if (9<=month<=11) {
+    month_param = 2;    //가을
+  } else {
+    month_param = 3;    //겨울
+  }
   //날씨API
   //console.log('날씨데이터: ',healing.WeatherApp);
   var sql = 'SELECT * FROM healings WHERE season=?';
-  conn.query(sql, month_params, function (err, rows, fields){
+  conn.query(sql, month_param, function (err, rows, fields){
       //season
     rows.forEach((row, index)=>{
       dataList.push(row);
     });
     healingData = dataList[Math.floor(Math.random()*dataList.length)]; 
+    console.log(healingData.video_src);
     // console.log(JSON.stringify(healingData));
     // console.log(JSON.parse(JSON.stringify(healingData)));
 
@@ -34,36 +44,49 @@ router.get('/', function(req, res) {
   });
 });
 
-// router.post("/:weather", async (req, res) => {
-//   const { weather } = req.params;
-//   console.log('서버에서 weather값: ',weather);
-//   if (weather == "Rain") {
-//     weather_param = 1;  // 비
-//   }
-//   else if (weather == "Snow") {
-//     weather_param = 2;  // 눈
-//   } else {
-//     weather_param = 0;  // 맑음, 안개 등..
-//   }
-//   //season
-//   let today = new Date();
-//   let month_param = today.getMonth()+1;
-//   var sql = 'SELECT * FROM healings WHERE weather=? AND season=?';
-//   conn.query(sql, weather_param, month_param, function (err, rows, fields){
-//     rows.forEach((row, index)=>{
-//       dataList.push(row);
-//     });
-//     healingData = dataList[Math.floor(Math.random()*weatherList.length)]; 
-//     var healingString = JSON.stringify(healingData);
-//     var responseData = JSON.parse(healingString)
-//     console.log(responseData);
-//   })
-//   console.log('sql done');
-//   // res.json(responseData);
-//   res.render('/', {
-//     healingData: responseData
-//   });
-// });
+router.post("/:weather", async (req, res) => {
+  const { weather } = req.params;
+  console.log('서버에서 weather값: ',weather);
+  if (weather == "Rain") {
+    weather_param = 1;  // 비
+  }
+  else if (weather == "Snow") {
+    weather_param = 2;  // 눈
+  } else {
+    weather_param = 0;  // 맑음, 안개 등..
+  }
+  //season
+  let today = new Date();
+  let month = today.getMonth()+1;
+  if (3<=month<=5) {
+    month_param = 0;    //봄
+  } else if (6<=month<=8) {
+    month_param = 1;    //여름
+  } else if (9<=month<=11) {
+    month_param = 2;    //가을
+  } else {
+    month_param = 3;    //겨울
+  }
+  dataList = []
+  var sql = 'SELECT * FROM healings WHERE weather=? AND season=?';
+  conn.query(sql, [weather_param, month_param], function (err, rows, fields){
+    console.log(rows);
+    rows.forEach((row, index)=>{
+      console.log(row);
+      dataList.push(row);
+    })
+    healingData = dataList[Math.floor(Math.random()*dataList.length)]; 
+    console.log('1',healingData);
+    console.log('2',healingData.video_src);
+
+    if(err) console.log('query is not excuted. select fail...\n' + err); // 만일 오류가 있으면 로그 띄우기
+    else  res.render('healing2', {
+      healingData: healingData
+    }); //오류가 안뜬다면 healing.ejs 로 rows값들을 list에 넣어 보낸다.
+  });
+    // var healingString = JSON.stringify(healingData);
+    // var responseData = JSON.parse(healingString)
+});
 
 
 var bodyParser = require('body-parser');
@@ -117,46 +140,46 @@ function apiCall() {
   })
 }
 
-// router.get('/advertisement', function(req, res) {
-//   let adsList=[];
-//   let sql = 'SELECT * FROM advertisement WHERE emotions=? AND season=? AND weather=?';
-//   conn.query(sql, function (err, rows, fields){
-//     rows.forEach((row, index)=>{
-//       //광고서비스는 기분과 날씨와 계절로 구분 (3-5 6-8 9-11 12-2)      
-//       if (3<=row.season<=5) {
-//         adsList.push(row);
-//       } else if (6<=row.season<=8) {
-//         adsList.push(row);
-//       } else if (9<=row.season<=11) {
-//         adsList.push(row);
-//       } else {
-//         adsList.push(row);
-//       }
-//     });
-//     //2. 랜덤 광고데이터 1개 저장.
-//     let adData = adsList[Math.floor(Math.random()*adsList.length)];
-//     // 안드쪽에서 update를 못해서 provised 칼럼을 update하는 방식은 못함.
-//     //android (emotion, src, service_name, address, detail_long, tel, navermap_url) 
-//     let insert_sql = 'INSERT INTO android (emotion, src, service_name, address, detail_long, tel, navermap_url) VALUES (?, ?, ?, ?, ?, ?, ?)';
-//     let params1 = adData.emotion;
-//     let params2 = adData.src;
-//     let params3 = adData.service_name;
-//     let params4 = adData.address;
-//     let params5 = adData.detail_long;
-//     let params6 = adData.tel;
-//     let params7 = adData.navermap_url;
+router.get('/advertisement', function(req, res) {
+  let adsList=[];
+  let sql = 'SELECT * FROM advertisement';
+  conn.query(sql, function (err, rows, fields){
+    rows.forEach((row, index)=>{
+      //광고서비스는 기분과 날씨와 계절로 구분 (3-5 6-8 9-11 12-2)      
+      if (3<=row.season<=5) {
+        adsList.push(row);
+      } else if (6<=row.season<=8) {
+        adsList.push(row);
+      } else if (9<=row.season<=11) {
+        adsList.push(row);
+      } else {
+        adsList.push(row);
+      }
+    });
+    //2. 랜덤 광고데이터 1개 저장.
+    let adData = adsList[Math.floor(Math.random()*adsList.length)];
+    // 안드쪽에서 update를 못해서 provised 칼럼을 update하는 방식은 못함.
+    //android (emotion, src, service_name, address, detail_long, tel, navermap_url) 
+    let insert_sql = 'INSERT INTO android (emotion, src, service_name, address, detail_long, tel, navermap_url) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    let params1 = adData.emotion;
+    let params2 = adData.src;
+    let params3 = adData.service_name;
+    let params4 = adData.address;
+    let params5 = adData.detail_long;
+    let params6 = adData.tel;
+    let params7 = adData.navermap_url;
 
-//     //3. android 테이블에 새 행 추가
-//     conn.query(insert_sql, [params1,params2,params3, params4, params5, params6, params7], function(err) { // sql를 실행하고 VALUES 으로 params를 보낸다.
-//       if(err) console.log('query is not excuted. insert fail...\n' + err);
-//       else console.log('a column of android table is inserted');
-//     });
+    //3. android 테이블에 새 행 추가
+    conn.query(insert_sql, [params1,params2,params3, params4, params5, params6, params7], function(err) { // sql를 실행하고 VALUES 으로 params를 보낸다.
+      if(err) console.log('query is not excuted. insert fail...\n' + err);
+      else console.log('a column of android table is inserted');
+    });
 
-//     res.render('advertisement', {
-//       adData: adData.src
-//     })
-//   });
-// });
+    res.render('advertisement', {
+      adData: adData.src
+    })
+  });
+});
 
 
 
