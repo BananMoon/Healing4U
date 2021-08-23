09d06ed9c6c472c0ffc349a1b502710b8b5c8987
moonz <banan99@naver.com>
2021-08-23 23:12:12 +0900

    error: healing page with DB

--- public/js/main/healing.js
+++ public/js/main/healing.js
@@ -1,33 +1,6 @@
-const weatherAPI = document.getElementById("weather");
-const API_KEY = "a0383cc2ac06a4b4308bb75c735f62d8"
+// healing.ejs에서 함수 뺌
 
-function onGeoOk(position){
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        const cityTag = document.querySelector('#weather span:first-child');
-        const weatherTag =  document.querySelector('#weather span:last-child');
-        const temperature = data.main.temp;
-        const weather = data.weather[0].main;
-        const city = data.name;
-        cityTag.innerText = `City: ${city}`;
-        weatherTag.innerText = `Weather: ${weather}
-        Temporary: ${temperature} °C`;
-        const weatherList = [city, weather, temperature];
-        //serviceAPI(weatherList);
-        console.log('api안에 ',weather);
-        // weatherAPI(weather);      
-    });
-}
-
-function onGeoError() {
-    alert("Can't find you. No weather for you.");
-}
-
-function weatherAPI() {
+function weatherAPI(weather) {
     $.ajax({
         type: "POST",
         url: `/${weather}`,
--- routes/index.js
+++ routes/index.js
@@ -5,38 +5,35 @@ const db_config = require('../config/db_info');
 const conn = db_config.init(); // db의 커넥터를 활성화 시킨다.
 db_config.connect(conn); //db에 커넥터를 연결해준다.
 
-
-rating = require('../public/js/main/rating');
-//error!!!!!!!!!!!! healing.ejs에서 가져와지지가 않음...healing = require('../views/healing');
-console.log('점수: ', rating);
 /*-------------패널 메인 화면------------*/
 /*GET home page */
 router.get('/', function(req, res) {
   let dataList = [];
   let today = new Date();
   let month = today.getMonth()+1;
-  if (3<=month<=5) {
+  console.log('달: ',month);
+  if (3<=month && month<=5) {
     month_param = 0;    //봄
-  } else if (6<=month<=8) {
+  } else if (6<=month && month<=8) {
     month_param = 1;    //여름
-  } else if (9<=month<=11) {
+  } else if (9<=month && month<=11) {
     month_param = 2;    //가을
   } else {
     month_param = 3;    //겨울
   }
   //날씨API
+  console.log('날씨 값: ',month_param);
   //console.log('날씨데이터: ',healing.WeatherApp);
   var sql = 'SELECT * FROM healings WHERE season=?';
+  dataList=[];
   conn.query(sql, month_param, function (err, rows, fields){
       //season
     rows.forEach((row, index)=>{
       dataList.push(row);
     });
     healingData = dataList[Math.floor(Math.random()*dataList.length)]; 
-    console.log(healingData.video_src);
-    // console.log(JSON.stringify(healingData));
-    // console.log(JSON.parse(JSON.stringify(healingData)));
 
+    console.log('화면에 전송될 데이터:', healingData);
     if(err) console.log('query is not excuted. select fail...\n' + err); // 만일 오류가 있으면 로그 띄우기
     else  res.render('healing', {
       healingData: healingData
@@ -44,55 +41,58 @@ router.get('/', function(req, res) {
   });
 });
 
-router.post("/:weather", async (req, res) => {
-  const { weather } = req.params;
-  console.log('서버에서 weather값: ',weather);
-  if (weather == "Rain") {
-    weather_param = 1;  // 비
-  }
-  else if (weather == "Snow") {
-    weather_param = 2;  // 눈
-  } else {
-    weather_param = 0;  // 맑음, 안개 등..
-  }
-  //season
-  let today = new Date();
-  let month = today.getMonth()+1;
-  if (3<=month<=5) {
-    month_param = 0;    //봄
-  } else if (6<=month<=8) {
-    month_param = 1;    //여름
-  } else if (9<=month<=11) {
-    month_param = 2;    //가을
-  } else {
-    month_param = 3;    //겨울
-  }
-  dataList = []
-  var sql = 'SELECT * FROM healings WHERE weather=? AND season=?';
-  conn.query(sql, [weather_param, month_param], function (err, rows, fields){
-    console.log(rows);
-    rows.forEach((row, index)=>{
-      console.log(row);
-      dataList.push(row);
-    })
-    healingData = dataList[Math.floor(Math.random()*dataList.length)]; 
-    console.log('1',healingData);
-    console.log('2',healingData.video_src);
+/*=======날씨api를 통해 받은 날씨값을 **초마다 해당 api를 호출하여 db를 조회. 해당 데이터와 함께 페이지 랜더링 ========*/
+// router.post("/:weather", async (req, res) => {
+//   const { weather } = req.params;
+//   console.log('서버에서 weather값: ',weather);
+//   if (weather == "Rain") {
+//     weather_param = 1;  // 비
+//   }
+//   else if (weather == "Snow") {
+//     weather_param = 2;  // 눈
+//   } else {
+//     weather_param = 0;  // 맑음, 안개 등..
+//   }
+//   //season
+//   let today = new Date();
+//   let month = today.getMonth()+1;
+//   if (3<=month && month<=5) {
+//     month_param = 0;    //봄
+//   } else if (6<=month && month<=8) {
+//     month_param = 1;    //여름
+//   } else if (9<=month && month<=11) {
+//     month_param = 2;    //가을
+//   } else {
+//     month_param = 3;    //겨울
+//   }
+//   dataList = []
+//   var sql = 'SELECT * FROM healings WHERE weather=? AND season=?';
+//   conn.query(sql, [weather_param, month_param], function (err, rows, fields){
+//     console.log(rows);
+//     rows.forEach((row, index)=>{
+//       console.log(row);
+//       dataList.push(row);
+//     })
+//     healingData = dataList[Math.floor(Math.random()*dataList.length)]; 
+//     console.log('1',healingData);
+//     console.log('2',healingData.video_src);
 
-    if(err) console.log('query is not excuted. select fail...\n' + err); // 만일 오류가 있으면 로그 띄우기
-    else  res.render('healing2', {
-      healingData: healingData
-    }); //오류가 안뜬다면 healing.ejs 로 rows값들을 list에 넣어 보낸다.
-  });
-    // var healingString = JSON.stringify(healingData);
-    // var responseData = JSON.parse(healingString)
-});
+//     if(err) console.log('query is not excuted. select fail...\n' + err); // 만일 오류가 있으면 로그 띄우기
+//     else  res.render('healing2', {
+//       healingData: healingData
+//     }); //오류가 안뜬다면 healing.ejs 로 rows값들을 list에 넣어 보낸다.
+//   });
+//     // var healingString = JSON.stringify(healingData);
+//     // var responseData = JSON.parse(healingString)
+// });
 
 
 var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
+/*========== 딥러닝 서버로부터 감정값을 받아서 db조회후 데이터값과 함께 광고서비스를 랜더링.=========*/
+// 우선 db조회 부터는 주석처리해놓음.
 router.get("/test", function(req,res){
   var get_body = req.body;
   console.log('dl서버:',get_body);
@@ -105,11 +105,11 @@ router.get("/test", function(req,res){
   //조회된 광고들에서 해당하는 weather의 데이터중 랜덤으로 해서 보여줌.
   // let today = new Date();
   // let month = today.getMonth()+1;
-  // if (3<=month<=5) {
+  // if (3<=month && month<=5) {
   //   month_param = 0;    //봄
-  // } else if (6<=month<=8) {
+  // } else if (6<=month && month<=8) {
   //   month_param = 1;    //여름
-  // } else if (9<=month<=11) {
+  // } else if (9<=month && month<=11) {
   //   month_param = 2;    //가을
   // } else {
   //   month_param = 3;    //겨울
@@ -140,21 +140,15 @@ function apiCall() {
   })
 }
 
+/* ========'/' 페이지에서 감정값을 받으면 advertisement.ejs를 랜더링할것임. ==========*/
+// 성공하면 해당 api는 지울거임
 router.get('/advertisement', function(req, res) {
   let adsList=[];
-  let sql = 'SELECT * FROM advertisement';
+  let sql = 'SELECT * FROM healings WHERE season=1';
   conn.query(sql, function (err, rows, fields){
     rows.forEach((row, index)=>{
       //광고서비스는 기분과 날씨와 계절로 구분 (3-5 6-8 9-11 12-2)      
-      if (3<=row.season<=5) {
-        adsList.push(row);
-      } else if (6<=row.season<=8) {
-        adsList.push(row);
-      } else if (9<=row.season<=11) {
-        adsList.push(row);
-      } else {
-        adsList.push(row);
-      }
+      adsList.push(row);
     });
     //2. 랜덤 광고데이터 1개 저장.
     let adData = adsList[Math.floor(Math.random()*adsList.length)];
@@ -176,11 +170,10 @@ router.get('/advertisement', function(req, res) {
     });
 
     res.render('advertisement', {
-      adData: adData.src
+      adData: adData.video_src
     })
   });
 });
-
 
 
 router.get('/rating', function(req, res) {
@@ -188,21 +181,8 @@ router.get('/rating', function(req, res) {
   })
 })
 
-// router.get("/rating/:result", async (req, res) => {
-//   const { data } = req.params;
-//   console.log('서버:',data);
-//   // goods = await Goods.findOne({ goodsId: goodsId });
-//   // res.json({ detail: goods });
-// });
 
-// router.post('/rating/good', function (req, res) {
-//   let data = req.body;
-//   console.log('서버:',data);
-// })
-
-//      <% let data = weatherList[Math.floor(Math.random()*weatherList.length)]; %>
-
-
+// rating 결과값을 전달
 router.get("/rating/:result", async (req, res) => {
   const { result } = req.params;
   console.log('서버에서 rating값: ',result);
