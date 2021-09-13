d74e0be36e2a3ec573978bba69700da6051434dd
moonz <banan99@naver.com>
2021-09-13 22:32:57 +0900

    소켓 통신 구현 중 w/딥러닝서버

--- public/js/main/healing.js
+++ public/js/main/healing.js
@@ -25,3 +25,63 @@ function weatherAPI(weather) {
               }
             });
 }
+
+function DLapi() {
+    let socket = io.connect('ec2-3-129-8-135.us-east-2.compute.amazonaws.com:8080');
+    socket.on('connect',function() {
+      console.log('Client has connected to the server!');
+    });
+    socket.on('msg',function(data) {
+      console.log('Received a message from the DLserver!',data);
+      send("data received!");
+    });
+    socket.on('disconnect',function() {
+      console.log('The client has disconnected!');
+    });
+    socket.on("error", function( error ) {
+      console.log("error: "+ error);
+    })
+    // Sends a message to the server via sockets
+    function send(message) {
+      socket.send('msg',message);
+    };
+
+
+    // socket.on('msg', function (data) {
+    //   console.log(data);
+    //   //$("#chatContent").append(`${data}<br>`);
+    // });
+
+    // $("#myChat").on("keyup", function () {
+    //   if (window.event.keyCode == 13) {
+    //     $("#chatContent").append(`Client : "${$(this).val()}" 보냅니다.<br>`);
+    //     socket.emit('msg', $(this).val());
+    //     $(this).val("");
+    //   }
+    // });
+  // $.ajax({
+  //   type: "GET",
+  //   url: `/healing/${weather}`,
+  //   data: {},
+  //   error: function(xhr, status, error) {
+  //       if (status == 404) {
+  //           alert("서버 응답 실패");
+  //           window.location.href = "/";
+  //       }
+  //   },
+  //   success: function(response) {
+  //     console.log(response.video_src);
+  //     // $("#video").attr("src", healingData["video_src"]);
+      
+  //     var videoSrc = document.getElementById('video');
+  //     console.log(videoSrc);
+  //     // videoSrc.innerHTML="<div id=\'bg\'><video muted autoplay loop src="+response.video_src+"></video></div>";
+  //     videoSrc.setAttribute("src", response.video_src);
+  //     // videoSrc.innerHTML = "<video muted autoplay loop src=" + response.video_src + "></video>"
+  //     var quote = document.getElementById('quote');
+  //     console.log(quote);
+  //     quote.innerHTML="<p>"+response.quote+"</p><p>"+response.quote_src+"</p>";
+      
+  //   }
+  // });
+}
